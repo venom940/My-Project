dfd4e00f7c3b4b8d356ec3bb81c863cfee59d69b
venom940 <venomlins400@icloud.com>
2023-07-12 11:34:36 +0100

    new

--- guess.py
+++ guess.py
@@ -2,15 +2,46 @@
 # importing random module
 import random
 
-#Correct  guess number 
+   # Start game level instruction
+user_level = input('....Choose Level.... \n\n 1. Beginner(1-20)'
+      '\n 2.Intermidiate (1-100) \n 3. Hard (1-1000) \n\n' 
+     'Choose Level' )
+    
+    # Assigning user_level into an integer
+user_level = int(user_level)
+level=0
+if user_level ==1:
+   level=20
+elif user_level ==2:
+   level=100
+elif user_level ==3:
+   level=1000
+else: 
+     print('Invalid User Input')
+     exit(1)
 
-guess_num = random.randint
+
+
+#Correct  guess number 
+chances=0
+guess_num = random.randint(1,level)
 i=0
 while i<5:
- user_input = int( input('Guess a number') )
- if user_input==guess_num:
-    print('Correct')
- else:
-    print('Try Again')
- i=i+1
+   user_input = int( input(f'Guess a number(1-{level}):') )
+   chances=chances+1
+      
+   if user_input==guess_num:
+    print('You won, and you used ', chances ,'chances')
+    break
+   else:
+      if user_input>level:
+        print('Out Of Range')
+      elif user_input > guess_num:
+            print("Your Guess is too High....Try Again")   
+      else:
+         print('Your Guess is too low....Try again')    
+  
+   i=i+1
+     
+print('..PROGRAM ENDED, THANKS FOR GUESSING')
  
--- trial.py
+++
@@ -1,6 +0,0 @@
-myguess = 10
-usernum = int(input('Enter Guess Number') )
-if usernum == myguess:
-    print("Good Guess")
-else:
-    print("Guess Again")
