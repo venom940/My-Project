32b15a68683d9c1d4cb941bb412581422789b296
venom940 <venomlins400@icloud.com>
2023-07-12 11:34:15 +0100

    new changes

---
+++ trial.py
@@ -0,0 +1,6 @@
+myguess = 10
+usernum = int(input('Enter Guess Number') )
+if usernum == myguess:
+    print("Good Guess")
+else:
+    print("Guess Again")
--- try.py
+++
@@ -1,8 +0,0 @@
-# Correct guess number
-guess_num = 9
-user_input = int( input('Guess a number') )
-if user_input==guess_num:
-    print('Correct')
-else:
-    print('Try Again')
-    
